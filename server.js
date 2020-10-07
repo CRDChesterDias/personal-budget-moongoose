f80e1a531b924bb75104305ac4741bd8a085437b
CRDChesterDias <dias.chester.r@hotmail.com>
2020-10-07 02:34:57 -0400

    Added cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
+
 app.use('/', express.static('public'))
+app.use(cors());
 
 const budget = {
 }
