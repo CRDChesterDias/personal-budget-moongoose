e434d5c3ff9be48c114d0016d9d5929e40a4aefc
CRDChesterDias <dias.chester.r@hotmail.com>
2020-10-30 15:50:36 -0400

    Added the quiz08 code

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const model = require("./models/budget_model");
 const cors = require('cors');
 
 const app = express();
@@ -8,17 +10,41 @@ const port = 3000;
 app.use('/', express.static('public'))
 app.use(cors());
 
-const budget = {
-}
+
+let url = 'mongodb://localhost:27017/mongodb_budget_data';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=>{
+            console.log("Connected to the database");
+        })
+        .catch((connectionError)=>{
+            console.log(connectionError)
+        });
 
 app.get('/hello',(req,resp)=>{
     resp.send('Hello World!')
 });
 
-app.get('/budget',(req,resp)=>{
-    //resp.json(budget)
-    resp.sendFile(__dirname + '/budget.json')
+app.get('/budget',async (req,resp)=>{
+    try{
+        const budget = await model.find();
+        resp.send(budget);
+    }
+    catch(exception){
+        console.log(exception);
+    }
 
+});
+
+app.post('/insertData', async (req, res) => {
+    try{
+        const data = new model({title: req.query.title, budget: req.query.budget, color: req.query.color}); 
+        const result = await data.save();
+        res.send(result);
+    }
+    catch(exception){
+        console.log(exception);
+    }
 });
 
 app.listen(port,()=>{
